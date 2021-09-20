ef9760cdff35c7543fe71a2decb4e1982681b0fb
David Thacher <davidethacher@gmail.com>
2021-09-20 00:31:30 -0400

    Create trigger.groovy

---
+++ trigger.groovy
@@ -0,0 +1,18 @@
+/* 
+ * File:   trigger.groovy
+ * Author: David Thacher
+ * License: GPL 3.0
+ *
+ * Created on September 19, 2021
+ */
+
+import java.nio.channels.FileChannel;
+
+// Map shared memory buffer
+def map = new RandomAccessFile("/tmp/LED_Matrix.mem", "rw").getChannel().map(FileChannel.MapMode.READ_WRITE, 0, new File("/tmp/LED_Matrix.mem").length())
+
+// Set pixel
+map.put((Byte) 4, (Byte) 255)
+
+// Trigger send_frame
+map.put((Byte) 0, (Byte) 1)
