5aa97c31cde598855087a1eddef84e6b6a5012bc
David Thacher <davidethacher@gmail.com>
2021-09-20 02:55:02 -0400

    Create trigger.py

---
+++ trigger.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+'''
+ File:   trigger.py
+ Author: David Thacher
+ License: GPL 3.0
+ 
+ Created on September 19, 2021
+ ''' 
+ 
+import mmap
+import os
+
+f = open("/tmp/LED_Matrix.mem", "r+b")
+mm = mmap.mmap(f.fileno(), length=0, access=mmap.ACCESS_WRITE)
+
+# Set pixel
+mm.seek(4)
+mm.write_byte(255)
+
+# Trigger send_frame
+mm.seek(0)
+mm.write_byte(1)
+
+# Wait for trigger to finish
+while True:
+	mm.seek(0)
+	if mm.read_byte() == 0:
+		break
+
+# Trigger send_frame with VLAN 13
+mm.seek(2)
+mm.write_byte(13)
+mm.seek(0)
+mm.write_byte(2)
+	
