636ee88dbd64982560deb8e7df5fc43a4a386b62
David Thacher <davidethacher@gmail.com>
2021-09-21 03:17:20 -0400

    Update trigger_remote.c

--- trigger_remote.c
+++ trigger_remote.c
@@ -33,6 +33,7 @@ int main(int argc, char **argv) {
 	int sockfd;
 	struct sockaddr_in serv_addr; 
 	struct packet p;
+	uint8_t data[8];
 
 	if (argc != 2) {
 		printf("Usage: %s <ip of server>\n", argv[0]);
@@ -52,11 +53,39 @@ int main(int argc, char **argv) {
 	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 		return errno;
     
+    	// Set pixel
+    	p.command = 3;
+    	p.marker = 0x09202021;
+    	p.size = 7;
+    	memset(data, 0, 7);
+    	data[4] = 255;
+	transfer(sockfd, 1, &p, sizeof(p));
+	transfer(sockfd, 1, data, p.size);
+    
     	// Trigger send_frame
     	p.command = 1;
     	p.marker = 0x09202021;
     	p.size = 0;
 	transfer(sockfd, 1, &p, sizeof(p));
+    
+    	// Trigger send_frame with VLAN 13
+    	p.command = 0;
+    	p.marker = 0x09202021;
+    	p.size = 2;
+    	memset(data, 0, 2);
+    	data[0] = 13;
+	transfer(sockfd, 1, &p, sizeof(p));
+	transfer(sockfd, 1, data, p.size);
+	
+	// Get the number of Rows
+    	p.command = 2;
+    	p.marker = 0x09202021;
+    	p.size = 0;
+    	memset(data, 0, 2);
+	transfer(sockfd, 1, &p, sizeof(p));
+	transfer(sockfd, 0, data, 8);
+	
+	printf("ROWS: %d, COLS %d\n", (int) (*((uint64_t *) data) >> 32), (int) (*((uint64_t *) data) & 0xFFFFFFFF));
     
 	return 0;
 }
