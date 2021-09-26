0ee340230f32eb4f5758bff6e3737942e28144e7
David Thacher <davidethacher@gmail.com>
2021-09-25 21:31:12 -0400

    Create trigger_remote.groovy

---
+++ trigger_remote.groovy
@@ -0,0 +1,21 @@
+/* 
+ * File:   trigger_remote.groovy
+ * Author: David Thacher
+ * License: GPL 3.0
+ *
+ * Created on September 25, 2021
+ */
+
+import groovy.Matrix;
+import groovy.Matrix_RGB_t;
+
+// Using Matrix class
+def m = new Matrix("127.0.0.1")
+def p = new Matrix_RGB_t((byte) 255, (byte) 255, (byte) 255)
+m.set_pixel(0, 0, p)
+m.send_frame()
+m.send_frame(13)
+rows = m.get_rows()
+cols = m.get_columns()
+m.fill(new Matrix_RGB_t((byte) 255, (byte) 255, (byte) 255))
+m.send_frame()
