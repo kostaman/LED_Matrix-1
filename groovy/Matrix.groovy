43972856a88ace5c72496d5233881cefc8064a35
David Thacher <davidethacher@gmail.com>
2021-09-25 21:24:20 -0400

    Update Matrix.groovy

--- groovy/Matrix.groovy
+++ groovy/Matrix.groovy
@@ -20,11 +20,24 @@ class Matrix {
 		map.put(0, (Byte) 4)
 		while (map.get(0));
 		cols = (map.get(2) << 8) + map.get(3)
+		isNet = false
 	}
 		
-	/*Matrix(InetAddress addr) {
-		
-	}*/
+	Matrix(InetAddress address) {
+		addr = address
+		isNet = true
+		def s = new Socket(addr, 8080)
+		s.withStreams { istream, ostream ->
+			def data = [0x09, 0x20, 0x20, 0x21, 2, 0 ] as byte[]
+			ostream.write(data, 0, data.size())
+			ostream.flush()
+			istream.read(data, 0, 4)
+			cols = data[3] << 24 | data[2] << 16 | data[1] << 8 | data[0]
+			istream.read(data, 0, 4)
+			rows = data[3] << 24 | data[2] << 16 | data[1] << 8 | data[0]
+		}
+		s.close()
+	}
 		
 	def get_rows() {
 		return 16
@@ -35,15 +48,37 @@ class Matrix {
 	}
 		
 	def send_frame() {
-		map.put(0, (byte) 1)
-		while(map.get(0));
+		if (!isNet) {
+			map.put(0, (byte) 1)
+			while(map.get(0));
+		}
+		else {
+			def s = new Socket(addr, 8080)
+			s.withStreams { istream, ostream ->
+				def data = [0x09, 0x20, 0x20, 0x21, 1, 0 ] as byte[]
+				ostream.write(data, 0, data.size())
+				ostream.flush()
+			}
+			s.close()
+		}
 	}
 		
 	def send_frame(int vlan_id) {
-		map.put(2, (byte) (vlan_id & 0xFF))
-		map.put(1, (byte) (vlan_id >> 8 & 0xFF))
-		map.put(0, (byte) 2)
-		while(map.get(0));
+		if (!isNet) {
+			map.put(2, (byte) vlan_id)
+			map.put(1, (byte) (vlan_id >> 8))
+			map.put(0, (byte) 2)
+			while(map.get(0));
+		}
+		else {
+			def s = new Socket(addr, 8080)
+			s.withStreams { istream, ostream ->
+				def data = [0x09, 0x20, 0x20, 0x21, 0, 2, vlan_id, vlan_id >> 8] as byte[]
+				ostream.write(data, 0, data.size())
+				ostream.flush()
+			}
+			s.close()
+		}
 	}
 	
 	def set_pixel(int x, int y, Matrix_RGB_t pixel) {
@@ -52,29 +87,87 @@ class Matrix {
 	}
 	
 	def set_pixel_raw(int x, int y, Matrix_RGB_t pixel) {
-		map.put(y * cols + x + 4, pixel.red)
-		map.put(y * cols + x + 5, pixel.green)
-		map.put(y * cols + x + 6, pixel.blue)
+		if (!isNet) {
+			map.put(y * cols + x + 4, pixel.red)
+			map.put(y * cols + x + 5, pixel.green)
+			map.put(y * cols + x + 6, pixel.blue)
+		}
+		else {
+			def v = y * rows + x as int
+			def s = new Socket(addr, 8080)
+			s.withStreams { istream, ostream ->
+				def data = [0x09, 0x20, 0x20, 0x21, 3, 7, v, v >> 8, v >> 16, v >> 24, pixel.red, pixel.green, pixel.blue] as byte[]
+				ostream.write(data, 0, data.size())
+				ostream.flush()
+			}
+			s.close()
+		}
+	}
+	
+	def set_pixel(int x, int y, Matrix_RGB_t[] pixels) {
+		if (!isNet)
+			for (Matrix_RGB_t pixel : pixels)
+				set_pixel(x, y, pixel)
+		else {
+			def v = y * rows + x as int
+			def s = new Socket(addr, 8080)
+			s.withStreams { istream, ostream ->
+				def data = [0x09, 0x20, 0x20, 0x21, 3, 4 + (pixels.size() * 3), v, v >> 8, v >> 16, v >> 24] as byte[]
+				ostream.write(data, 0, data.size())
+				ostream.flush()
+				for (Matrix_RGB_t pixel : pixels) {
+					data[0] = pixel.red
+					data[1] = pixel.green
+					data[2] = pixel.bleu
+					ostream.write(data, 0, 3)
+					ostream.flush()
+				}
+			}
+			s.close()
+		
+		}
 	}
 	
 	def fill(Matrix_RGB_t pixel) {
-		for (int y = 0; y < rows; y++) {
-			for (int x = 0; x < cols; x++) {
-				map.put(y * cols + x + 4, pixel.red)
-				map.put(y * cols + x + 5, pixel.green)
-				map.put(y * cols + x + 6, pixel.blue)
+		if (!isNet) {
+			for (int y = 0; y < rows; y++) {
+				for (int x = 0; x < cols; x++) {
+					map.put(y * cols + x + 4, pixel.red)
+					map.put(y * cols + x + 5, pixel.green)
+					map.put(y * cols + x + 6, pixel.blue)
+				}
 			}
+		}
+		else {
+			def s = new Socket(addr, 8080)
+			s.withStreams { istream, ostream ->
+				def data = [0x09, 0x20, 0x20, 0x21, 5, 3, pixel.red, pixel.green, pixel.blue] as byte[]
+				ostream.write(data, 0, data.size())
+				ostream.flush()
+			}
+			s.close()
 		}
 	}
 	
 	def clear() {
-		fill(Matrix_RGB_t())
+		fill(new Matrix_RGB_t((byte) 0, (byte) 0, (byte) 0))
 	}
 	
 	def set_brightness(byte brightness) {
-		map.put(1, brightness)
-		map.put(0, (byte) 5)
-		while(map.get(0));
+		if (!isNet) {
+			map.put(1, brightness)
+			map.put(0, (byte) 5)
+			while(map.get(0));
+		}
+		else {
+			def s = new Socket(addr, 8080)
+			s.withStreams { istream, ostream ->
+				def data = [0x09, 0x20, 0x20, 0x21, 6, 1, brightness] as byte[]
+				ostream.write(data, 0, data.size())
+				ostream.flush()
+			}
+			s.close()
+		}
 	}
 	
 	protected def map_pixel(int x, int y) {
@@ -85,8 +178,10 @@ class Matrix {
 	}
 	
 	protected MappedByteBuffer map
+	protected InetAddress addr
 	protected int rows
 	protected int cols
+	protected boolean isNet
 	
 	protected class pixel_cord {
 		pixel_cord() {
