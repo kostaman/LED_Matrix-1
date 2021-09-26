22b3327e18c79faf0ac0240647d5448437656e50
David Thacher <davidethacher@gmail.com>
2021-09-25 20:03:01 -0400

    Create Matrix_RGB_t.groovy

---
+++ groovy/Matrix_RGB_t.groovy
@@ -0,0 +1,21 @@
+/* 
+ * File:  Matrix_RGB_t.groovy
+ * Author: David Thacher
+ * License: GPL 3.0
+ *
+ * Created on September 25, 2021
+ */
+ 
+ package groovy;
+
+class Matrix_RGB_t {
+	Matrix_RGB_t(byte r, byte g, byte b) {
+		red = r
+		green = g
+		blue = b
+	}
+
+	byte red
+	byte green
+	byte blue
+}
