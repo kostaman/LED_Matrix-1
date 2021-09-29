334bf3faae46387036c736b5146cf9050c9333cf
David Thacher <davidethacher@gmail.com>
2021-09-29 11:28:09 -0400

    Create trigger_remote.cpp

---
+++ trigger/trigger_remote.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   trigger_remote.cpp
+ * Author: David Thacher
+ * License: GPL 3.0
+ *
+ * Created on September 29, 2021
+ */
+ 
+#include "../C++/Matrix.h"
+
+int main(int argc, char **argv) {
+
+	{
+		Matrix *m = new Matrix();
+		Matrix_RGB_t p(255, 255, 255);
+		m->set_pixel(0, 0, p);
+		m->send_frame();
+		m->send_frame(13);
+		m->fill(p);
+		m->send_frame();
+	}
+
+	{
+		Matrix *m = new Matrix("127.0.0.1");
+		Matrix_RGB_t p(255, 255, 255);
+		m->set_pixel(0, 0, p);
+		m->send_frame();
+		m->send_frame(13);
+		m->fill(p);
+		m->send_frame();
+	}
+
+	return 0;
+}
