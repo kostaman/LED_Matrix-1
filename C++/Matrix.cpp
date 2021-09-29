db13c4c544b4d713943227275f91e43fc76128a5
David Thacher <davidethacher@gmail.com>
2021-09-29 03:43:35 -0400

    Create Matrix.cpp

---
+++ C++/Matrix.cpp
@@ -0,0 +1,56 @@
+/* 
+ * File:   Matrix.cpp
+ * Author: David Thacher
+ * License: GPL 3.0
+ *
+ * Created on September 29, 2021
+ */
+ 
+ Matrix::Matrix() {
+ 
+ }
+ 
+ Matrix::Matrix(char *address) {
+ 
+ }
+ 
+ void Matrix::send_frame() {
+ 
+ }
+ 
+ void Matrix::send_frame(uint16_t vlan_id) {
+ 
+ }
+ 
+void set_pixel(uint32_t x, uint32_t y, Matrix_RGB_t pixel) {
+
+}
+
+void Matrix::set_pixel(uint32_t x, uint32_t y, Matrix_RGB_t *pixels) {
+
+}
+
+void Matrix::set_pixel_raw(uint32_t x, uint32_t y, Matrix_RGB_t pixel) {
+
+}
+
+void Matrix::set_pixel_raw(uint32_t x, uint32_t y, Matrix_RGB_t *pixels) {
+
+}
+		
+void Matrix::fill(Matrix_RGB_t pixel) {
+
+}
+
+void Matrix::clear() {
+
+}
+		
+void Matrix::set_brightness(uint8_t brightness) {
+
+}
+
+inline void map_pixel(int *x, int *y) {
+
+}
+ 
